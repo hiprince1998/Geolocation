5db98524314cd59f4bcd1dcf17146a7cdf890342
@princekumar <princekumarcena@gmail.com>
2017-11-01 01:39:12 +0530

    Geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GoogleMaps, GoogleMapsEvent, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  constructor(public navCtrl: NavController,private googleMaps:GoogleMaps) {
 
-  constructor(public navCtrl: NavController) {
+ let map =new GoogleMap('map');
 
   }
 
+
+   
 }
+
+
