72f6e0eea017ea0cd916a1e737691c97b3825e86
@princekumar <princekumarcena@gmail.com>
2017-11-01 22:57:04 +0530

    change first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild,ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GoogleMaps, GoogleMapsEvent, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
-
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+@ViewChild('map') mapref:ElementRef;
   
-  constructor(public navCtrl: NavController,private googleMaps:GoogleMaps) {
+  constructor(public navCtrl: NavController,private googleMaps:GoogleMaps) {}
 
- let map =new GoogleMap('map');
+ionViewDidLoad(){
+   
+}
 
-  }
-
-
+ 
    
 }
 
